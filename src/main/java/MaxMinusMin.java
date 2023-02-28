8cfe626c91d7a9be45a9f86c260430638e79c296
BlackBeard1O1 <henos.job@gmail.com>
2023-02-28 02:13:28 +0000

    CollectionMaxMin Lab

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -1,5 +1,8 @@
 
 import java.util.List;
+import java.util.Collections;
+
+import javafx.print.Collation;
 
 public class MaxMinusMin {
     /**
@@ -10,6 +13,9 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        int x = Collections.max(nums);
+        int y = Collections.min(nums);
+
+        return x - y;
     }
 }
